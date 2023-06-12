9fae7a7d5c8285ad8d832e7be9959f207acbefb9
Valerie Asmaralda <90575572+asmaraldav@users.noreply.github.com>
2023-06-12 12:46:47 +0200

    uploaded solutions up to 1-20

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,8 @@
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to programming!");
+        System.out.println("Quackie says hi!");
+    }
+}
